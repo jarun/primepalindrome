jarun/dslib

Change -W to -Wextra. Makes more sense.
Signed-off-by: Arun Prakash Jana <engineerarun@gmail.com>

Makefile
@@ -1,7 +1,7 @@
 CC = gcc
 CPP = g++
-CFLAGS = -Wall -Werror -g
-CPPFLAGS = -Wall -Werror -g
+CFLAGS = -Wall -Wextra -Werror -g
+CPPFLAGS = -Wall -Wextra -Werror -g
 
 objects = log.o dlist.o tree.o queue.o stack.o
 

jarun/primepalindrome

Makefile
@@ -1,5 +1,5 @@
 CC = gcc
-CFLAGS = -W -Wall -Werror -O3
+CFLAGS = -Wall -Wextra -Werror -O3
 
 all: primepalindrome
 